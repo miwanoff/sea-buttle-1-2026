035ccfff0ad00e2cad2f9db8fdefa86dc4e72296
miwanoff <marina.iva.noffa@gmail.com>
2026-05-02 12:54:59 +0300

    Add angle

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const gameOptionContainer = document.querySelector("#game-option");
 const rotateButton = document.querySelector("#rotate");
-
+let angle = 0;
 
 function rotate() {
-  const optionShips = gameOptionContainer.children;
-  for (const ship of optionShips) {
-    console.log(ship.className);
-  }
+  // const optionShips = gameOptionContainer.children;
+  //   for (const ship of optionShips) {
+  //     console.log(ship.className);
+  //     ship.style.transform = "rotate(90deg)";
+  //   }
+  const optionShips = Array.from(gameOptionContainer.children);
+  angle = angle === 0 ? 90 : 0;
+
+  optionShips.forEach(
+    (optionShip) => (optionShip.style.transform = `rotate(${angle}deg)`),
+  );
 }
 
 rotateButton.addEventListener("click", rotate);
